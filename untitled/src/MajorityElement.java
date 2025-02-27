8a8bb62ad1c2f7617bc6cdd0eeba58efc7f29df2
Saqibbhat321 <saqib.bhat.x@gmail.com>
2025-02-28 00:36:54 +0530

    Majority Element using Boyer Moore Voting algo

---
+++ untitled/src/MajorityElement.java
@@ -0,0 +1,28 @@
+public class MajorityElement {
+
+    // Using Boyer-Moore Voting Algorithm.
+
+    public static int majorityElement(int nums[]) {
+
+        int count = 0;
+        int candidate = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0 && candidate != nums[i]) {
+                candidate = nums[i];
+                count = 1;
+            } else if (candidate == nums[i]) {
+                count++;
+            } else
+                count--;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 3, 4, 2, 4, 4, 2, 4, 4};
+
+        int result = majorityElement(nums);
+        System.out.println("The majority element is: " + result);
+    }
+}
