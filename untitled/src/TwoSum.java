0ac779dc2503efe145823e77612f74006c832c56
Saqib Rashid Bhat <120316006+Saqibbhat321@users.noreply.github.com>
2025-04-26 21:38:39 +0530

    Update TwoSum.java

--- untitled/src/TwoSum.java
+++ untitled/src/TwoSum.java
@@ -1,1 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
 
+public class TwoSumExample {
+
+    public static int[] twoSum(int target, int[] arr) {
+        Map<Integer, Integer> mapStore = new HashMap<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            // ERROR 1: We should check for complement BEFORE putting current element in map
+            // Otherwise, we might use the same element twice (when target = 2*arr[i])
+            int complement = target - arr[i];
+            
+            if (mapStore.containsKey(complement)) {
+                return new int[]{mapStore.get(complement), i};
+            }
+            
+            // Correct position to put the current element in the map
+            mapStore.put(arr[i], i);
+        }
+            return null; 
+    }
+
+    public static void main(String[] args) {
+        int target = 10;
+        int[] arr = {2, 3, 5, 7, 8};
+        
+        int[] result = twoSum(target, arr);
+        if (result != null) {
+            System.out.println("Indices: " + java.util.Arrays.toString(result));
+            System.out.println("Values: " + arr[result[0]] + " + " + arr[result[1]] + " = " + target);
+        } else {
+            System.out.println("No solution found");
+        }
+    }
+}
