3bef1e16d95a4776e696eeecdef1ff56cf324eb4
Saqib Rashid Bhat <120316006+Saqibbhat321@users.noreply.github.com>
2025-04-28 00:28:04 +0530

    Update ContainsDuplicate.java

--- untitled/src/ContainsDuplicate.java
+++ untitled/src/ContainsDuplicate.java
@@ -1,1 +1,33 @@
 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+            //Time complexity O(n)
+        Set<Integer> store = new HashSet<>();
+        for(int num :nums)
+        {
+            if(store.contains(num)){
+                return true;
+            }
+        store.add(num);
+        }
+        return false;
+
+        //Time Complexity O(n^2) Brute Force
+
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         for(int j=0;j<i;j++)
+    //         {
+    //             if(nums[i]==nums[j])
+    //             {
+
+    //             return true;
+    //             }
+            
+    //         }
+        
+    // }
+        // return false;
+}
+}
